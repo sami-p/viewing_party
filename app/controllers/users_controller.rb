a97507c49247fd8bc93de606e760bcc451256e37
Elliot Olbright <elliotolbright@gmail.com>
2021-08-24 16:22:53 -0600

    add user login page link to welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
+
   def create
     user = user_params
     user[:username] = user[:username].downcase
@@ -9,7 +11,24 @@ class UsersController < ApplicationController
     flash[:success] = "Welcome, #{new_user.username}!"
     redirect_to root_path #change to dashboard
   end
+
+  def login
+    user = User.find_by(username: params[:username])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id 
+      flash[:success] = "Welcome, #{user.username}!"
+      redirect_to root_path #change to user dashboard 
+    else 
+      flash[:error] = "Sorry, please fill in a valid username and password."
+      render :login_form
+    end 
+  end 
+
+  def login_form 
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
