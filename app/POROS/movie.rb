259bfc66b954e952241511dc793a63bb8bd26763
Elliot Olbright <elliotolbright@gmail.com>
2021-08-27 20:53:20 -0600

    add center text to details page

--- app/POROS/movie.rb
+++ app/POROS/movie.rb
@@ -2,12 +2,19 @@ class Movie
   attr_reader :title, 
               :vote_average,
               :id,
-              :runtime
+              :runtime,
+              :image
+              :backdrop_path
 
   def initialize(data)
-    @title        = data[:title]
-    @vote_average = data[:vote_average]
-    @id           = data[:id]
-    @runtime      = data[:runtime]
+    @title            = data[:title]
+    @vote_average     = data[:vote_average]
+    @id               = data[:id]
+    @runtime         = data[:runtime]
+    @backdrop_path = data[:backdrop_path]
   end 
+
+  def render_image
+    "https://image.tmdb.org/t/p/w500#{@backdrop_path}" unless @poster.nil?
+  end
 end
