df690744b0a6222e3689f85b69b60396f3d6ad3a
sami-p <sami.peterson14@gmail.com>
2021-08-29 12:31:03 -0400

    Add access to movie and friends

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,5 +1,10 @@
 class PartiesController < ApplicationController
 
   def new
-  end 
+    @movie = MovieDbService.get_movie(params[:id])
+    @friends = current_user.friends
+  end
+
+  def create
+  end
 end
