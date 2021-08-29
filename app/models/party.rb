76bdf5e85b9fa8eb39b7881c3e088dda4fe0a620
sami-p <sami.peterson14@gmail.com>
2021-08-29 12:31:38 -0400

    Add presence verification of datetime

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,8 @@
 class Party < ApplicationRecord
   belongs_to :host, class_name: 'User'
+
   has_many :party_guests, dependent: :destroy
   has_many :users, through: :party_guests
+
+  validates_presence_of :date_time
 end
