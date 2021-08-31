4c3b72e85e0405017b23d3992ed3072ebcedbc77
Amanda McGuire <aruwart001@gmail.com>
2021-08-31 15:36:45 -0600

    update for params and adding method to controller

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,8 @@
 class UserMailer < ApplicationMailer
   def viewing_email
     @user = params[:user]
-    @host = params[:host]
-    @movie = params[:movie]
+    @party = params[:party]
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Movie Viewing Party Invite!')
-  end
-
-  def add_to_controller
-    UserMailer.with(user: @friend, host: host, movie: movie).welcome_email.deliver_later
   end
 end
