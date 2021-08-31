153928722d7252fe1510a6e7736568781f0f75ef
Elliot Olbright <elliotolbright@gmail.com>
2021-08-31 14:53:27 -0600

    rough styling draft 1

--- app/POROS/movie_search.rb
+++ app/POROS/movie_search.rb
@@ -1,9 +1,16 @@
 class MovieSearch
   attr_reader :id,
-              :title
+              :title,
+              :poster
 
   def initialize(data)
-    @title = data[:title]
-    @id    = data[:id]
+    @title  = data[:title]
+    @id     = data[:id]
+    @poster = data[:poster_path]
+  end
+
+
+  def render_image
+    "https://image.tmdb.org/t/p/w500#{@poster}" unless @poster.nil?
   end
 end
--- app/POROS/movies.rb
+++ app/POROS/movies.rb
@@ -1,11 +1,17 @@
 class Movies
   attr_reader :title,
               :vote_average,
-              :id
+              :id,
+              :poster
 
   def initialize(data)
     @title        = data[:title]
     @vote_average = data[:vote_average]
     @id           = data[:id]
+    @poster       = data[:poster_path]
+  end
+
+  def render_image
+    "https://image.tmdb.org/t/p/w500#{@poster}" unless @poster.nil?
   end
 end
