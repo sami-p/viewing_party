3ae98b6fe6ee8818450e8f699da6e548299c571d
Amanda McGuire <aruwart001@gmail.com>
2021-08-31 13:57:40 -0600

    Update params and add method to controller after new party merge

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,13 @@
 class UserMailer < ApplicationMailer
   def viewing_email
-    @user = params[:user] # need to add multiple params for friend/host?
-    @url  = 'http://example.com/login' # need to be viewing url?
+    @user = params[:user]
+    @host = params[:host]
+    @movie = params[:movie]
+    @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Movie Viewing Party Invite!')
+  end
+
+  def add_to_controller
+    UserMailer.with(user: @friend, host: host, movie: movie).welcome_email.deliver_later
   end
 end
